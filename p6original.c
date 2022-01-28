f1311057056c113a2c7bbe59c1badc9791ed4a1c
harshithama18 <97089315+harshithama18@users.noreply.github.com>
2022-01-28 03:26:22 +0000

    p5original

--- p6original.c
+++ p6original.c
@@ -27,3 +27,20 @@ int main()
   output(x,y);
   return 0;
 }
+
+
+
+
+//for(i=1;n>=i;i++)           //n=5
+   // sum=sum+i;
+//sum=+1  sum=1
+//sum=1+2    sum=3
+//sum=3+3  sum=6
+//sum=6+4   sum=10
+//sum=10+5    sum=15
+
+
+
+//sum=1+1 =2         // n=3
+//sum=2+2=4
+//sum=4+3=7
